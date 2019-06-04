1fc6ead6741ce419b413ea108a3d714d3dc1404d
zn <zn993160060@icloud.com>
2019-06-04 17:05:59 +0800

    abc

--- modules/wiki.js
+++ modules/wiki.js
@@ -31,7 +31,7 @@ exports.wiki_baike = function(req,res){
 
 exports.baike_next = function(req,res){
     var food_search =  req.query.food_search;
-    db.query('select * from baike_next where culture like "%?%" ',[food_search], (err, result) => {
+    db.query('select * from baike_next where culture like "%麻婆豆腐%" ',[food_search], (err, result) => {
         if (err) {
             res.send({
                 status: 0,
